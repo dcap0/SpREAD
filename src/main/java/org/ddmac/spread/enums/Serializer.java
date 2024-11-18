cc856e64e89111274e3b0b18f02771d90d896550
Dennis Capone <dennis.d.cap@gmail.com>
2024-11-18 13:47:29 -0500

    added javadocs

--- src/main/java/org/ddmac/spread/enums/Serializer.java
+++ src/main/java/org/ddmac/spread/enums/Serializer.java
@@ -1,7 +1,19 @@
 package org.ddmac.spread.enums;
 
+/**
+ * Enum for user to provide the serializer they're using.
+ */
 public enum Serializer {
+    /**
+     * com.google.code.gson
+     */
     GSON,
+    /**
+     * com.fasterxml.jackson.core:jackson-databind
+     */
     JACKSON,
+    /**
+     * kotlinx.serialize
+     */
     KOTLIN
 }
--- src/main/java/org/ddmac/spread/repositorydata/RepositoryData.java
+++ src/main/java/org/ddmac/spread/repositorydata/RepositoryData.java
@@ -2,6 +2,16 @@ package org.ddmac.spread.repositorydata;
 
 import org.ddmac.spread.enums.Serializer;
 
+/**
+ * Record holding metadata about annotated interface
+ *
+ * @param interfacePackage Package the interface resides in
+ * @param repoSimpleName Simple name of the repository class.
+ * @param entityName Simple name of the entity associated with the repository.
+ * @param reqPath User provided path for the api endpoint.
+ * @param serializer User provided enum for the serializer they use
+ */
+
 public record RepositoryData(
         String interfacePackage,
         String repoSimpleName,
