371c67139aeb459ec9472754187eddd7c45fd8a7
Dennis Capone <dennis.d.cap@gmail.com>
2024-12-04 19:51:03 -0500

    made annotation a submodule

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "SpREAD"
-
+include(":annotation")
--- testing/src/main/java/org/ddmac/spreadtest/data/TestRepository.java
+++
@@ -1,8 +0,0 @@
-package org.ddmac.spread.test.data;
-
-import org.ddmac.spread.SpREAD;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@SpREAD
-public interface TestRepository extends JpaRepository<TestEntity,Long> {
-}
